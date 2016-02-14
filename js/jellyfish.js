c7c985f7349c06e1d0dd731fa5638a56fd9e49e7
Peter Downs <peterldowns@gmail.com>
2016-02-14 13:37:30 -0500

    Style fixes.

--- js/jellyfish.js
+++ js/jellyfish.js
@@ -1,19 +1,12 @@
+var setupScene = function() {
+    var WIDTH = window.innerWidth;
+    var HEIGHT = window.innerHeight;
+    var VIEW_ANGLE = 45;
+    var ASPECT = WIDTH / HEIGHT;
+    var NEAR = 0.1;
+    var FAR = 10000;
 
-
-setupScene();
-
-function setupScene() { 
-
-    //************SETUP SCENE****************
-
-    var WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight,
-    VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 0.1,
-    FAR = 10000;
-
-    // Create the scene and set the scene size. 
+    // Create the scene and set the scene size.
     scene = new THREE.Scene();
 
     // Create a renderer and add it to the DOM = Direct Object Model.
@@ -31,7 +24,7 @@ function setupScene() {
     camera.position.z = 200;
     // camera.lookAt(new THREE.Vector3(10, 0, 0));
     camera.lookAt(scene.position);
-    scene.add(camera); 
+    scene.add(camera);
 
     // Create an event listener that resizes the renderer with the browser window.
     window.addEventListener('resize', function() {
@@ -47,194 +40,192 @@ function setupScene() {
     spotLight.position.set(-100, 100, -100);
     spotLight.castShadow = true;
     scene.add(spotLight);
-    //ambient light 
+    //ambient light
     scene.add(new THREE.AmbientLight(0x666666));
 
-    //build axes 
+    //build axes
     var l = 10;
     axes = buildAxes(l);
     scene.add(axes);
 
-    //mouse controls 
+    //mouse controls
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     //GUI
     //addDatGui();
-
 }
 
-draw_cut_lines();
 
-function draw_cut_lines(){
-
+var draw_cut_lines = function() {
     a = 15;
     b = 6;
-    gamma = 60*Math.PI/180;//must be greater than 45!
-    t = (a-b)/(2*Math.cos(gamma)); //hypotenuse 
-    h = 2*t*Math.sin(gamma) + a + b; //height/width of unit cell 
-    n = 100; //nxn array of points (of 4x4 small cells)
+    gamma = 60*Math.PI/180; //must be greater than 45!
+    t = (a-b)/(2*Math.cos(gamma)); //hypotenuse
+    h = 2*t*Math.sin(gamma) + a + b; //height/width of unit cell
+    n = 20; //nxn array of points (of 4x4 small cells)
+    inc = [a, t*Math.sin(gamma), b, t*Math.cos(gamma)];
 
-    inc = [a,t*Math.sin(gamma),b,t*Math.cos(gamma)];
-
-        // col 1
+    // col 1
     base_coords=[
         0,0,
         0,inc[0],
         inc[3],inc[0]+inc[1],
         inc[3],inc[0]+inc[1]+inc[2],
-        //col 2
- //   base_coords_2 = [
+    //col 2
+    //   base_coords_2 = [
         inc[2]+inc[3],inc[0]+inc[1]+inc[2],
         inc[2]+inc[3],inc[0]+inc[1],
         inc[0],inc[0],
         inc[0],0,
-        
-        //col 3
-//    base_coords_3 = [
+
+    //col 3
+    //    base_coords_3 = [
         inc[0]+inc[1],inc[3],
         inc[0]+inc[1],inc[2]+inc[3],
         inc[0]+inc[1]-inc[3],inc[1]+inc[2]+inc[3],
         inc[0]+inc[1]-inc[3],inc[0]+inc[1]+inc[2]+inc[3],
-        // col 4
-  //  base_coords_4 = [
+    // col 4
+    //  base_coords_4 = [
         inc[0]+inc[1]-inc[3]+inc[0],inc[0]+inc[1]+inc[2]+inc[3],
         inc[0]+inc[1]-inc[3]+inc[0],inc[1]+inc[2]+inc[3],
         inc[0]+inc[1]+inc[2],inc[2]+inc[3],
-        inc[0]+inc[1]+inc[2],inc[3]];
-    //need to connect all horizontals and all vertical lines together 
-    // --> this is an indexing problem 
+        inc[0]+inc[1]+inc[2],inc[3]
+    ];
+    // need to connect all horizontals and all vertical lines together
+    // --> this is an indexing problem
 
     len = base_coords.length;
-    var foldmap = new THREE.Geometry();    
-    for(var i = 0; i < n; i++) {  
-        for(k=0; k<n; k++){
-            for(var j=0; j < base_coords.length; j+=2){
-                foldmap.vertices.push(new THREE.Vector3(h*k + base_coords[j], h*i + base_coords[j+1],0));
-            }     
-        }   
+    var foldmap = new THREE.Geometry();
+    for (var i = 0; i < n; i++) {
+        for (k=0; k<n; k++) {
+            for (var j=0; j < base_coords.length; j+=2) {
+                foldmap.vertices.push(
+                    new THREE.Vector3(h*k + base_coords[j],
+                                      h*i + base_coords[j+1],
+                                      0));
+            }
+        }
     }
 
-    var foldmap_odd = new THREE.Geometry(); 
+    var foldmap_odd = new THREE.Geometry();
     j = 1
-        for(k = 1; k < foldmap.vertices.length - 1; k++){
-             if (k == 16*n*j) {              
-                foldmap_odd.vertices.push(foldmap.vertices[k-1])                
-                foldmap_odd.vertices.push(foldmap.vertices[k])  
-                j += 1;
-                k-=1;              
-             }
-             else {
-                foldmap_odd.vertices.push(foldmap.vertices[k])
-                }                             
+    for (k = 1; k < foldmap.vertices.length - 1; k++) {
+        if (k == 16*n*j) {
+            foldmap_odd.vertices.push(foldmap.vertices[k-1])
+            foldmap_odd.vertices.push(foldmap.vertices[k])
+            j += 1;
+            k -= 1;
+         } else {
+            foldmap_odd.vertices.push(foldmap.vertices[k])
         }
-
-
-//for line pieces, add two vertices at a time for desired lines 
-    var foldmap_horiz = new THREE.Geometry();
-    for(k = 0; k < foldmap.vertices.length - 7; k+=8){
-        foldmap_horiz.vertices.push(foldmap.vertices[k])
-        foldmap_horiz.vertices.push(foldmap.vertices[k+7])
-        //
-        foldmap_horiz.vertices.push(foldmap.vertices[k+1])
-        foldmap_horiz.vertices.push(foldmap.vertices[k+6])
-        //
-        foldmap_horiz.vertices.push(foldmap.vertices[k+2])
-        foldmap_horiz.vertices.push(foldmap.vertices[k+5])
-        //
-        foldmap_horiz.vertices.push(foldmap.vertices[k+3])
-        foldmap_horiz.vertices.push(foldmap.vertices[k+4])
     }
-    
+
+
+    //for line pieces, add two vertices at a time for desired lines
+    var foldmap_horiz = new THREE.Geometry();
+    for (k = 0; k < foldmap.vertices.length - 7; k += 8) {
+        foldmap_horiz.vertices.push(foldmap.vertices[k]);
+        foldmap_horiz.vertices.push(foldmap.vertices[k+7]);
+        //
+        foldmap_horiz.vertices.push(foldmap.vertices[k+1]);
+        foldmap_horiz.vertices.push(foldmap.vertices[k+6]);
+        //
+        foldmap_horiz.vertices.push(foldmap.vertices[k+2]);
+        foldmap_horiz.vertices.push(foldmap.vertices[k+5]);
+        //
+        foldmap_horiz.vertices.push(foldmap.vertices[k+3]);
+        foldmap_horiz.vertices.push(foldmap.vertices[k+4]);
+    }
+
     var foldmap_trap = new THREE.Geometry();
-    for(k = 4; k < foldmap.vertices.length - 11; k+=8){
-        if(foldmap.vertices[k].x > foldmap.vertices[k+4].x){ //up to next row 
-            foldmap_trap.vertices.push(foldmap.vertices[k])                
-            foldmap_trap.vertices.push(foldmap.vertices[k])                
-        }
-        else{
-        foldmap_trap.vertices.push(foldmap.vertices[k])
-        foldmap_trap.vertices.push(foldmap.vertices[k+7])
-        //            
-        foldmap_trap.vertices.push(foldmap.vertices[k+1])
-        foldmap_trap.vertices.push(foldmap.vertices[k+6])
-        //            
-        foldmap_trap.vertices.push(foldmap.vertices[k+2])
-        foldmap_trap.vertices.push(foldmap.vertices[k+5])
-        //            
-        foldmap_trap.vertices.push(foldmap.vertices[k+3])
-        foldmap_trap.vertices.push(foldmap.vertices[k+4])
-        
+    for (k = 4; k < foldmap.vertices.length - 11; k += 8) {
+        if (foldmap.vertices[k].x > foldmap.vertices[k+4].x) { //up to next row
+            foldmap_trap.vertices.push(foldmap.vertices[k]);
+            foldmap_trap.vertices.push(foldmap.vertices[k]);
+        } else{
+          foldmap_trap.vertices.push(foldmap.vertices[k]);
+          foldmap_trap.vertices.push(foldmap.vertices[k+7]);
+          //
+          foldmap_trap.vertices.push(foldmap.vertices[k+1]);
+          foldmap_trap.vertices.push(foldmap.vertices[k+6]);
+          //
+          foldmap_trap.vertices.push(foldmap.vertices[k+2]);
+          foldmap_trap.vertices.push(foldmap.vertices[k+5]);
+          //
+          foldmap_trap.vertices.push(foldmap.vertices[k+3]);
+          foldmap_trap.vertices.push(foldmap.vertices[k+4]);
         }
     }
 
     var foldmap_trap_vert = new THREE.Geometry();
-    for(k = 1;k<foldmap.vertices.length - 5; k+=8){
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k])
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+1])
+    for (k = 1; k < foldmap.vertices.length - 5; k += 8) {
+        foldmap_trap_vert.vertices.push(foldmap.vertices[k]);
+        foldmap_trap_vert.vertices.push(foldmap.vertices[k+1]);
         //
-        if (k < 16*n*(n-1)){
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+2])
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+16*n - 1])
-        //
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+3])
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+16*n + 7 - 1])
-    }
-        //
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+4])
-        foldmap_trap_vert.vertices.push(foldmap.vertices[k+5])
+        if (k < 16*n*(n-1)) {
+          foldmap_trap_vert.vertices.push(foldmap.vertices[k+2]);
+          foldmap_trap_vert.vertices.push(foldmap.vertices[k+16*n - 1]);
+          //
+          foldmap_trap_vert.vertices.push(foldmap.vertices[k+3]);
+          foldmap_trap_vert.vertices.push(foldmap.vertices[k+16*n + 7 - 1]);
+        }
+        foldmap_trap_vert.vertices.push(foldmap.vertices[k+4]);
+        foldmap_trap_vert.vertices.push(foldmap.vertices[k+5]);
     }
 
     material = new THREE.LineBasicMaterial({
-                //size: .5,
-                color: 0x997799
-            });
+      //size: .5,
+      color: 0x997799,
+    });
 
     points = new THREE.Line(foldmap, material,THREE.LinePieces);
     points_horiz = new THREE.Line(foldmap_horiz, material, THREE.LinePieces);
     points_trap = new THREE.Line(foldmap_trap, material, THREE.LinePieces);
     points_trap_vert = new THREE.Line(foldmap_trap_vert, material, THREE.LinePieces);
     scene.add(points); // square sides (list of vector pairs (x1,y1,z1),(x2,y2,z2))
-    scene.add(points_horiz); // square top/bottom 
+    scene.add(points_horiz); // square top/bottom
     scene.add(points_trap);
     scene.add(points_trap_vert);
 
-    //put all points in a long 1D list for output
-
-coords = [];
-    length = points.geometry.vertices.length //all the points 
-    for (var i=0; i<length;i++){
+    // put all points in a long 1D list for output
+    coords = [];
+    length = points.geometry.vertices.length //all the points
+    for (var i = 0; i < length;i++) {
         coords.push(points.geometry.vertices[i].x);
         coords.push(points.geometry.vertices[i].y);
     }
-    for (var i=0; i<points_horiz.geometry.vertices.length;i++){
+    for (var i = 0; i < points_horiz.geometry.vertices.length; i++) {
         coords.push(points_horiz.geometry.vertices[i].x);
         coords.push(points_horiz.geometry.vertices[i].y);
-    }  
-    for (var i=0; i<points_trap.geometry.vertices.length;i++){
+    }
+    for (var i = 0; i < points_trap.geometry.vertices.length; i++) {
         coords.push(points_trap.geometry.vertices[i].x);
         coords.push(points_trap.geometry.vertices[i].y);
     }
-    for (var i=0; i<points_trap_vert.geometry.vertices.length;i++){
+    for (var i = 0; i < points_trap_vert.geometry.vertices.length; i++) {
         coords.push(points_trap_vert.geometry.vertices[i].x);
         coords.push(points_trap_vert.geometry.vertices[i].y);
     }
-    
 
-    var blobparts = ccl2dxf(coords,0,20,0,20);
+    var blobparts = ccl2dxf(coords, 0, 20, 0, 20);
     var blob = new Blob(blobparts, {type: "text/plain"});
     saveAs(blob, "kiri.dxf");
-
 }
 
-animateScene();
 
-function animateScene() {
+// Does this need to be a function?
+var renderScene = function() {
+  renderer.render(scene, camera);
+}
+
+var animateScene = function() {
     points.geometry.verticesNeedUpdate = true; // ask for an update
     controls.update();
     renderScene();
     requestAnimationFrame(animateScene);
 }
 
-function renderScene() {
-    renderer.render( scene, camera );
-}
+// Here, call the functions all at once.
+setupScene();
+draw_cut_lines();
+animateScene();
